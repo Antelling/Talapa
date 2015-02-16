23d9666aa617e26bf5f19f1999ee3e2b5db237cc
Anthony Dellinger <dibillilia@gmail.com>
2015-02-15 23:22:09 -0500

    Fixed file reading bug, and added some file skipping features

--- bin/index.js
+++ bin/index.js
@@ -2,52 +2,61 @@
 
 var send = require('../lib/languageChooser.js');
 var colors = require('colors');
-
-//code to read and watch files and directories
-//read a file
+var dirsToIgnore = ' ';
 var fs = require('fs');
 var path = require('path');
+var skipDIR = false;
 
 var isDefault = true;
 var contains = ' ' + process.argv.join(' ') + ' ';
 
-if (contains.indexOf(' -p') != -1 || contains.indexOf('--print') != -1) {
+if (contains.indexOf(' -ot ') != -1 || contains.indexOf('--onlyThis') != -1) {
+	skipDIR = true;
+}    //skip sass
+
+if (contains.indexOf(' -xs ') != -1 || contains.indexOf('--ignoreSASS') != -1) {
+	var skipSass = true;
+}    //skip sass
+
+if (contains.indexOf(' -xc ') != -1 || contains.indexOf('--ignoreCoffeescript') != -1) {
+	var skipCoffee = true;
+}  //skip coffeescript
+
+if (contains.indexOf(' -xt ') != -1 || contains.indexOf('--ignoreTalapa') != -1) {
+	var skipTal = true;
+}    //skip talapa
+
+process.argv.forEach(function(arg){
+	if (arg.substr(0, 3) === '-x-') {
+		dirsToIgnore = dirsToIgnore + ' ' + arg.substr(4) + ' ';
+	}
+}); //set up the dirs to skip
+
+if (contains.indexOf(' -p ') != -1 || contains.indexOf('--print') != -1) {
 	var print = true;
 }  //should I print?
 
-if (contains.indexOf(' -f') != -1 || contains.indexOf('--file') != -1){
+if (contains.indexOf(' -f ') != -1 || contains.indexOf('--file') != -1){
 	convertFile(process.argv[2], process.argv[3]);
 	isDefault = false;
 }  //Is it a file? 
 
-if (contains.indexOf(' -d') != -1 || contains.indexOf('--directory') != -1) {
+if (contains.indexOf(' -d ') != -1 || contains.indexOf('--directory') != -1) {
 	convertDir(process.argv[2], process.argv[3]);
 	isDefault = false;
 }   //I guess it's a directory
 
-if (contains.indexOf(' -xm') != -1 || contains.indexOf('--ignoreMarkdown') != -1) {
+if (contains.indexOf(' -xm ') != -1 || contains.indexOf('--ignoreMarkdown') != -1) {
 	var skipMarkdown = true;
 }   //skip markdown
 
-if (contains.indexOf(' -xs') != -1 || contains.indexOf('--ignoreSASS') != -1) {
-	var skipSass = true;
-}    //skip sass
-
-if (contains.indexOf(' -xc') != -1 || contains.indexOf('--ignoreCoffeescript') != -1) {
-	var skipCoffee = true;
-}  //skip coffeescript
-
-if (contains.indexOf(' -xt') != -1 || contains.indexOf('--ignoreTalapa') != -1) {
-	var skipTal = true;
-}    //skip talapa
-
-if (contains.indexOf(' -wf') != -1 || contains.indexOf('--watchFile') != -1) {
+if (contains.indexOf(' -wf ') != -1 || contains.indexOf('--watchFile') != -1) {
 	convertFile(process.argv[2], process.argv[3]);
 	watchFile(process.argv[2], process.argv[3]);
 	isDefault = false;
 }  //now we have to watch this thing
 
-if (contains.indexOf(' -wd') != -1 || contains.indexOf('--watchDirectory') != -1) {
+if (contains.indexOf(' -wd ') != -1 || contains.indexOf('--watchDirectory') != -1) {
 	watchDirectory(process.argv[2], process.argv[3]);
 	isDefault = false;
 }   //now we have to watch a whole lot of things
@@ -62,7 +71,7 @@ function convertFile(origFile, compFile, same) {
 	switch(path.extname(origFile)){
 		case '.talapa':
 			if (!skipTal) {
-				console.log('compiling'.blue, origFile);
+				console.log('compiling'.cyan, origFile);
 				compFile = compFile.replace('.talapa', '.html');
 				code = send.recieve(data.toString(), false, false, path.dirname(origFile));
 				same = false;
@@ -73,7 +82,7 @@ function convertFile(origFile, compFile, same) {
 		case '.sass':
 		case '.scss':
 			if (!skipSass) {
-				console.log('compiling'.blue, origFile);
+				console.log('compiling'.cyan, origFile);
 				compFile = compFile.replace('.sass', '.css');
 				if (!sass) { var sass = require('../lib/sass.js'); }
 				code = sass.compile(data.toString());
@@ -85,7 +94,7 @@ function convertFile(origFile, compFile, same) {
 		case '.coffee':
 		case '.litcoffee':
 			if (!skipCoffee) {
-				console.log('compiling'.blue, origFile);
+				console.log('compiling'.cyan, origFile);
 				compFile = compFile.replace('.coffee', '.js').replace('.litcoffee', '.js');
 				if (!coffee) { var coffee = require('../lib/coffeescript.js'); }
 				code = coffee.compile(data.toString());
@@ -97,7 +106,7 @@ function convertFile(origFile, compFile, same) {
 		case '.md':
 		case '.markdown':
 			if (!skipMarkdown) {
-				console.log('compiling'.blue, origFile);
+				console.log('compiling'.cyan, origFile);
 				compFile = compFile.replace('.md', '.html').replace('.markdown', '.html');
 				if (!marked) { var marked = require('marked'); }
 				code = marked(data.toString());
@@ -122,13 +131,32 @@ function convertFile(origFile, compFile, same) {
 function convertDir(origDir, compDir) {
 	var files = fs.readdirSync(origDir);
 	files.forEach(function(file){
-		if (path.extname(file)) {
-			convertFile(path.join(origDir, file), path.join(compDir, file));
-		} else {
-			if (!fs.existsSync(path.join(compDir, file))) {
-				fs.mkdirSync(path.join(compDir, file));
+		var stats = fs.lstatSync(path.join(origDir, file));
+		if (file.charAt(0) !== '_') {
+			if (stats.isFile()) {
+				convertFile(path.join(origDir, file), path.join(compDir, file));
+			} else if (stats.isDirectory()){
+				if (skipDIR) {  //we shouldnt skip anything
+					console.log('skipping'.blue, file);
+				} else if (dirsToIgnore.indexOf( ' ' + file + ' ') != -1) {  //it is a directory we should ignore
+					console.log('skipping'.blue, file);	
+				} else {  //it's normal
+					if (!fs.existsSync(path.join(compDir, file))) {
+						fs.mkdirSync(path.join(compDir, file));
+					}
+					convertDir(path.join(origDir, file), path.join(compDir, file));
+				}
+			} else {
+				//user is doing not good things. They have objects in their filesystem that are not files or directories.
+				console.log();
+				console.log('=======very bad thing========'.red);
+				console.log(file, 'is not a file or directory. It will be skipped.');
+				console.log('============================='.red);
+				console.log();
+				console.log('skipping'.blue, file);
 			}
-			convertDir(path.join(origDir, file), path.join(compDir, file)); 
+		} else {
+			console.log('skipping'.blue, file);
 		}
 	});
 }
@@ -156,14 +184,29 @@ function watchDirectory (origDir, compDir) {
 	var same = false;
 	if (origDir == compDir) { same = true; }
 	files.forEach(function(file) {
+		var stats = fs.lstatSync(path.join(origDir, file));
 		if (file.charAt(0) !== '_') {
-			if (path.extname(file)) {
+			if (stats.isFile()) {
 				convertFile(path.join(origDir, file), path.join(compDir, file), same);
 				watchFile(path.join(origDir, file), path.join(compDir, file), same);
+			} else if (stats.isDirectory()) {
+				if (skipDIR) {  //we should skip everything
+					console.log('skipping'.blue, file);
+				} else if (dirsToIgnore.indexOf(' ' + file + ' ') != -1) {  //it is a directory we should ignore
+					console.log('skipping'.blue, file);
+				} else {  //its normal
+					watchDirectory(path.join(origDir, file), path.join(compDir, file));
+				}
 			} else {
-				watchDirectory(path.join(origDir, file), path.join(compDir, file));
+				//user is doing not good things. They have objects in their filesystem that are not files or directories.
+				console.log();
+				console.log('=======very bad thing========'.red);
+				console.log(file, 'is not a file or directory. It will be skipped.');
+				console.log('============================='.red);
+				console.log();
+				console.log('skipping'.blue, file);
 			}
-		} else{
+		} else {
 			console.log('skipping'.blue, file);
 		}
 	});
